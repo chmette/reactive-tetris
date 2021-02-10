3a1f320281c5ad50717663693f08621c3c818b22
Chrissi <cmette.online@googlemail.com>
2021-02-10 16:19:23 +0100

    add base costum hooks & components

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -3,8 +3,9 @@ import {StyledStage} from './styles/StyledStage';
 import Cell from './Cell';
 
 const Stage = ({ stage }) => {
-
+	
 	return (
+		
 		<StyledStage width={stage[0].length} height={stage.length}>
 			{stage.map(row => row.map((cell, x) => 
 				 <Cell key={x} type={cell[0]}/> 
---
+++ src/hooks/useStage.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { createStage } from "../gameHelpers";
+
+export const useStage = (player, resetPlayer) => {
+  const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+      return newStage;
+    };
+
+    setStage((prev) => updateStage(prev));
+  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
+
+  return [stage, setStage];
+};
