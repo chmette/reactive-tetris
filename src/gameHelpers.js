27bf1f81750779dea9102d5fff232c0099e7a0e1
Olha Halat <olha.halat@gmail.com>
2021-02-11 14:38:25 +0100

    commit before pulling

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -4,3 +4,4 @@ export const STAGE_HEIGHT = 20;
 export const createStage = () => Array.from(Array(STAGE_HEIGHT),()=>new Array(STAGE_WIDTH).fill([0,'clear']));
 
 console.log(createStage());
+
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -24,7 +24,7 @@ export const useStage = (player, resetPlayer) => {
     };
 
     setStage((prev) => updateStage(prev));
-  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
+  }, [player]);
 
   return [stage, setStage];
 };
