92f59d357780171647164489c371e1da511aff1d
Chrissi <cmette.online@googlemail.com>
2021-02-11 14:17:27 +0100

    add checkCollision

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 
-import {createStage} from '../gameHelpers';
+ import {createStage, checkCollision} from '../gameHelpers';
+
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
+//Hooks
 import {usePlayer} from '../hooks/usePlayer';
 import {useStage} from '../hooks/useStage';
 
@@ -16,21 +18,37 @@ const Tetris = () => {
 	const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer] = usePlayer();
-	const [stage, setStage] = useStage(player);
+	const [stage, setStage] = useStage(player, resetPlayer);
 
-	console.log('render!');
+	console.log('re-render!');
 
+      //move tetronimo left and right  
 	const movePlayer = dir => {
-		updatePlayerPos({x: dir, y: 0})
+		if (!checkCollision(player, stage, {x: dir, y: 0})) {
+			updatePlayerPos({x: dir, y: 0})
+		}
 	}
 
 	const startGame = () => {
+        console.log('start-game test')
+        //reset everything
 		setStage(createStage());
 		resetPlayer();
+		setGameOver(false);
 	}
 
 	const drop = () => {
-		updatePlayerPos({x: 0, y: 1, collided: false})
+		if (!checkCollision(player, stage, {x: 0, y: 1})) {
+			updatePlayerPos({x: 0, y: 1, collided: false})
+		} else {
+			// Game Over!
+			if (player.pos.y < 1) {
+				console.log("it's all over now");
+				setGameOver(true);
+				setDropTime(null);
+			}
+			updatePlayerPos({x: 0, y: 0, collided: true});
+		}
 	}
 
 	const dropPlayer = () => {
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -6,10 +6,12 @@ export const useStage = (player, resetPlayer) => {
 
   useEffect(() => {
     const updateStage = (prevStage) => {
+      //clear stage from prev render
       const newStage = prevStage.map((row) =>
         row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
       );
-
+      console.log("useEffect: clean stage,draw tetromino");
+      //draw the tetromino
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
@@ -20,11 +22,19 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
+
+      // collision checking
+      if (player.collided) {
+        resetPlayer();
+      }
+
       return newStage;
     };
 
     setStage((prev) => updateStage(prev));
-  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
+  }, [player, resetPlayer]);
+
+  // [player.collided, player.pos.x, player.pos.y, player.tetromino]
 
   return [stage, setStage];
 };
