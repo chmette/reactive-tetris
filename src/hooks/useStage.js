627f27190af0f2819f0c9bf444eca708cc9387ba
Chrissi <cmette.online@googlemail.com>
2021-02-15 09:48:04 +0100

    add sweepRows

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,22 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = (newStage) =>
+      newStage.reduce((acc, row) => {
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = (prevStage) => {
       //clear stage from prev render
       const newStage = prevStage.map((row) =>
@@ -26,6 +40,7 @@ export const useStage = (player, resetPlayer) => {
       // collision checking
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -36,5 +51,5 @@ export const useStage = (player, resetPlayer) => {
 
   // [player.collided, player.pos.x, player.pos.y, player.tetromino]
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
