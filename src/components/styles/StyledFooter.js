4ce1423c0064a2048bf9fede43a349db2c4e8635
Chrissi <cmette.online@googlemail.com>
2021-02-17 12:01:45 +0100

    modify footer style

--- src/components/styles/StyledFooter.js
+++ src/components/styles/StyledFooter.js
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 
-export const StyledFooter = styled.div`
+export const StyledFooter = styled.footer`
   background-color: #000;
   color: #fff;
-  height: 1.8rem;
   width: 100%;
-  padding: 0.8rem;
+  padding: 0.5rem 1rem 0 1rem;
   font-family: Arial, Helvetica, sans-serif;
   text-align: center;
   align-self: center;
-  font-size: 0.9rem;
+  font-size: 0.8rem;
 
   a {
     color: rgb(223, 173, 36);
@@ -17,10 +16,8 @@ export const StyledFooter = styled.div`
     font-weight: bold;
   }
 
-  @media screen and (max-width: 600px);
-   {
+  @media screen and (max-width: 600px) {
     font-size: 0.7rem;
-    height: 1.5rem;
   }
 `;
 //
